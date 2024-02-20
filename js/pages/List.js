6fe305d9cfb1f950a98b30cbd8ed242e2613dc2f
mariokirby1703 <60894103+mariokirby1703@users.noreply.github.com>
2024-02-20 13:08:49 +0100

    Update List.js
    Update the Submission Requirements / Rules

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,28 +100,22 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without the use of hacks. (Unlimited FPS is allowed, Physics Bypass is prohibited)
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record. It's prohibited to change the level in any way.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        The recording may be cut when reaching & respawning from a checkpoint, although the overall completion has to be done in one continuous run without leaving the level.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must also show the player hit the end trigger and the endscreen with the time shown, or the completion will be invalidated.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The recording must have clicks for extreme demon completions.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        The completion doesnt include any exploits (Secret Ways, Bugs) - Small skips are fine.
                     </p>
                 </div>
             </div>
